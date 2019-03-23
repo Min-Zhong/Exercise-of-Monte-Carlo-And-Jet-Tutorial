240a7e73f84237fca48b220027d815cb039b5bff
Min-Zhong <48853883+Min-Zhong@users.noreply.github.com>
2019-03-23 17:27:16 +0800

    Update Exercise1_Uniform.py

--- Exercise1_Uniform.py
+++ Exercise1_Uniform.py
@@ -17,3 +17,11 @@ def draw_hist(myList, Title, Xlabel, Ylabel, Xmin, Xmax, Ymin, Ymax):
     plt.show()
 
 draw_hist(NumberList1, 'Uniform Distribution between [0,1]', 'RandomNumber', 'Counts', 0.0, 1.0, 0.0, 35.0)
+
+NumberList2 = []
+for iran in range(1000):
+    ran2 = random.random()
+    ran3 = ran2*10 + 5.0
+    NumberList2.append(ran3)
+
+draw_hist(NumberList2, 'Uniform Distribution between [5,15]', 'RandomNumber', 'Counts', 5.0, 15.0, 0.0, 35.0)
